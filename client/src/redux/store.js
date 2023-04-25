e4e02892ae2cfb4877e8b0b1040fa7d7099c8eda
Adarsh Chavhan <130956407+adarshchavhan21@users.noreply.github.com>
2023-04-25 16:04:51 +0530

    persistor added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,24 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import { userReducer } from './userReducer';
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist'
+import persistStore from 'redux-persist/es/persistStore';
+
+const reducer = combineReducers({
+    user: userReducer
+});
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer
-    }
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
+});
+
+export const persistor = persistStore(store);
